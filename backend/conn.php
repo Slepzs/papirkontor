709002d1982528eee1bc5596ee106e1eefc9b1a5
Slepzs <slepzs@gmail.com>
2018-06-02 17:23:11 +0200

    no message

--- backend/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbServername = 'heidework.com.mysql';
-$dbusername = 'heidework_com';
-$dbpassword = 'Wx4z2n5h6s1';
-$dbname = 'heidework_com';
-
-
-$conn = mysqli_connect($dbServername, $dbusername, $dbpassword, $dbname);
